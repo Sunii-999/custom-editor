91ffffd4aba2d00d8e18a66fac3233fd9421e719
Sunii-999 <stijnwal01@gmail.com>
2025-08-31 02:19:58 +0200

    table implementation

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,7 +3,11 @@
 import { useEditor, EditorContent} from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 import TaskItem from "@tiptap/extension-task-item"
+import Table from "@tiptap/extension-table"
 import TaskList from "@tiptap/extension-task-list"
+import TableCell from "@tiptap/extension-table-cell"
+import TableHeader from "@tiptap/extension-table-header"
+import TableRow from "@tiptap/extension-table-row"
 
 export const Editor = () => {
 
@@ -16,13 +20,33 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            Table.configure({
+                resizable: true,
+            }),
+            TableHeader,
+            TableRow,
+            TableCell,
             TaskItem.configure({
                 nested:true,
             }),
             TaskList,
         ],
-        content: '<p>Hello World!</p>'
-    })
+        content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,    })
 
     return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
