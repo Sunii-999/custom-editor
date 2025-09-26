16305ccbad72f9ae8514d0e4ab99d25810178206
Sunii-999 <stijnwal01@gmail.com>
2025-09-26 16:02:01 +0200

    added image insertion button with url and upload

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -83,20 +83,7 @@ export const Editor = () => {
             ImageResize,
         ],
         content: `
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th colspan="3">Description</th>
-            </tr>
-            <tr>
-              <td>Cyndi Lauper</td>
-              <td>Singer</td>
-              <td>Songwriter</td>
-              <td>Actress</td>
-            </tr>
-          </tbody>
-        </table>
+        
       `,    })
 
     return (
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -15,7 +15,10 @@ import {
     RemoveFormattingIcon,
     ChevronDownIcon,
     HighlighterIcon,
-    Link2Icon
+    Link2Icon,
+    ImageIcon,
+    UploadIcon,
+    SearchIcon
 } from "lucide-react";
 
 import {cn} from "@/lib/utils";
@@ -26,22 +29,142 @@ import { Separator } from "@/components/ui/separator";
 import { type ColorResult, SketchPicker } from "react-color"
 import { type Level } from "@tiptap/extension-heading";
 
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+const AlignButton = () => {
+    const { editor } = useEditorStore();
+
+    const alignments = [
+        {
+            label
+        }
+    ]
+
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className="h-7 min-w-7 shrinks-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <HighlighterIcon className="size-4"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0">
+                <SketchPicker
+                    color = {value}
+                    onChange={onChange}
+                />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+const ImageButton = () => {
+    const { editor } = useEditorStore();
+    const [ isDialogOpen, setIsDialogOpen] = useState(false);
+    const [ imageUrl, setImageUrl] = useState("");
+
+    const onChange = (src: string) => {
+        editor?.chain().focus().setImage({src}).run();
+    };
+
+    const onUpload = () => {
+        const input = document.createElement("input");
+        input.type = "file";
+        input.accept = "image/*";
+
+        input.onchange = (e) => {
+            const file = (e.target as HTMLInputElement).files?.[0];
+            if (file) {
+                const imageUrl = URL.createObjectURL(file);
+                onChange(imageUrl)
+            }
+        }
+
+        input.click();
+    }
+
+    const handleImageUrlSubmit = () => {
+        if (imageUrl) {
+            onChange(imageUrl);
+            setImageUrl("");
+            setIsDialogOpen(false);
+        }
+    };
+
+    return(
+        <>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className="h-7 min-w-7 shrinks-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <ImageIcon className="size-4"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem onClick={onUpload}>
+                    <UploadIcon className="size-4 mr-2" />
+                        Upload
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick = {() => setIsDialogOpen(true)}>
+                    <SearchIcon className="size-4 mr-2" />
+                        Paste image url
+
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Insert image URL
+                    </DialogTitle>
+                    <Input
+                        placeholder="Insert image URL"
+                        value={imageUrl}
+                        onChange={(e) => setImageUrl(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                handleImageUrlSubmit()
+                            }
+                        }}
+                    />
+                </DialogHeader>
+                <DialogFooter>
+                <Button onClick={handleImageUrlSubmit}>
+                    Insert
+                </Button>
+            </DialogFooter>
+            </DialogContent>
+        </Dialog>
+        </>
+    )
+
+}
+
 
 const LinkButton = () => {
     const { editor } = useEditorStore();
-    const [ value, setValue] = useState(editor?.getAttributes("link").href || "");
+    const [ value, setValue] = useState("");
 
-    const onChange =  (href: string) => {
-        editor?.chain().focus(). extendMarkRange("link").setLink({ href }).run();
+    const onChange = (href: string) => {
+        editor?.chain().focus().extendMarkRange("link").setLink({ href }).run();
         setValue("");
     }
 
@@ -370,7 +493,7 @@ export const Toolbar = () => {
             <HighlightColorButton/>
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
             <LinkButton/>
-            {/* TODO: Image */}
+            <ImageButton/>
             {/* TODO: Align */}
             {/* TODO: Line Height */}
             {/* TODO: List */}
