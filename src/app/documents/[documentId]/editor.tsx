9d8aed3941b776f8f45b9922defcd04acb421774
Sunii-999 <stijnwal01@gmail.com>
2025-10-02 16:39:43 +0200

    Added a ruler with marker movement

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,6 +21,7 @@ import TextStyle from "@tiptap/extension-text-style"
 import { FontSizeExtension } from "@/extensions/font-size"
 import { LineHeightExtension } from "@/extensions/line-height"
 
+import { Ruler } from "./ruler"
 
 
 import { useEditorStore } from "@/store/use-editor-store"
@@ -31,6 +32,7 @@ export const Editor = () => {
   const { setEditor } = useEditorStore()
 
     const editor = useEditor ({
+      immediatelyRender: false,
       onCreate({editor}) {
         setEditor(editor)
       },
@@ -99,6 +101,7 @@ export const Editor = () => {
 
     return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+          <Ruler />
             <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
                 <EditorContent editor={editor} />
             </div>
