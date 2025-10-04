5c93da0719608fd5e7cfec1b5f984a9e262a4daa
Sunii-999 <stijnwal01@gmail.com>
2025-10-04 15:03:08 +0200

    convex connection realtime database update to store documents

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+
 import Link from "next/link"
 import { Navbar } from "./navbar"
 
 import { TemplateGallery } from "./template-gallery"
+import { useQuery } from "convex/react"
+import { api } from "../../../convex/_generated/api"
 
 const Home = () => {
+  const documents = useQuery(api.documents.getDocuments)
+
+  if (!documents) {
+    return <div>Loading...</div>
+  }
+
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -11,6 +22,9 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   )
